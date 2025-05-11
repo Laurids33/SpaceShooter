d4c6a0332cd2410cebe131755c9b42ac93dca16e
Laurids33 <laurids.dechow@gmail.com>
2025-05-11 08:12:30 +0200

    Make player move and spawn bullets

---
+++ Assets/Scipts/Spieler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Spieler : MonoBehaviour
+{
+    readonly float eingabeFaktor = 10;
+    public GameObject[] geschoss = new GameObject[3];
+
+    void Update()
+    {
+        float yEingabe = Input.GetAxis("Vertical");
+        float yNeu = transform.position.y + yEingabe * eingabeFaktor * Time.deltaTime;
+        if (yNeu > 4.75f) yNeu = 4.75f;
+        else if (yNeu < -4.75f) yNeu = -4.75f;
+        transform.position = new Vector3(transform.position.x, yNeu, 0);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("a");
+            for(int i = 0; i < 3; i++)
+            {
+                if (!geschoss[i].activeSelf)
+                {
+                    Vector3 positionNeu = new Vector3(transform.position.x + 1.7f, transform.position.y, 0);
+                    geschoss[i].transform.position = positionNeu;
+                    geschoss[i].SetActive(true);
+                    break;
+                }
+            }
+        }
+    }
+}
