f35b16bdbf6f9c61baede4452df4aa7a730410d7
Laurids33 <laurids.dechow@gmail.com>
2025-05-11 08:32:46 +0200

    Make spaceShttles move

---
+++ Assets/Scipts/GefahrGewinn.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GefahrGewinn : MonoBehaviour
+{
+    public float xAenderungBasis;
+    float xAenderung;
+
+    void Start()
+    {
+        xAenderungBasis = 0.6f * Time.deltaTime;
+        xAenderung = 0.6f * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - xAenderung, transform.position.y, 0);
+        if (transform.position.x < -9.0f)
+        {
+            transform.position = new Vector3(Random.Range(10.0f, 19.0f), Random.Range(-4.5f, 4.5f), 0);
+            xAenderungBasis *= 1.01f;
+            xAenderung = xAenderungBasis * Random.Range(0.9f, 1.1f);
+        }
+    }
+}
